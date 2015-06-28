a62f9755d79b889989660142e5d2f72731341cb6
adamdry <adamdryit@gmail.com>
2015-06-28 17:49:57 +0100

    Updated read me and package.json

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# node-logger
+# node-easy-logger
 A node.js module with a simple api to log debug/error information to the disk or a database (currently just mysql)
---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+    node-easy-logger - A node.js module with a simple api to log debug/error information to the disk or a database (currently just mysql)
+
+    Copyright (C) 2015 - Adam Dry IT Limited
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//var co = require('co');
+var app = require('express')();
+var http = require('http').Server(app);
+
